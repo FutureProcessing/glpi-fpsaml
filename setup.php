078ef8b30421a62d19203afc6db4c36d5bd8124f
Andżelika Kustra <akustra@future-processing.com>
2021-09-17 12:15:15 +0200

    Setup file has been updated.

--- setup.php
+++ setup.php
@@ -63,8 +63,8 @@ function plugin_version_fpsaml()
  */
 function plugin_fpsaml_check_prerequisites()
 {
-    if (version_compare(GLPI_VERSION, '0.84', 'lt') || version_compare(GLPI_VERSION, '9.5.5', 'gt')) {
-        echo "This plugin requires GLPI >= 0.84 and GLPI <= 9.5.5";
+    if (version_compare(GLPI_VERSION, '0.84', 'lt') || version_compare(GLPI_VERSION, '9.6', '>=')) {
+        echo "This plugin requires GLPI >= 0.84 and GLPI < 9.6";
         return false;
     }
 
